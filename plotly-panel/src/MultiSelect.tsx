5209295898daaf4f8cc9317c831ce7b36c39b116
Christine Karas <christine.karas@ni.com>
2020-11-24 11:09:54 -0600

    make MultiSelectValueEditor a function component

--- plotly-panel/src/MultiSelect.tsx
+++ plotly-panel/src/MultiSelect.tsx
@@ -1,68 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FieldConfigEditorProps, SelectFieldConfigSettings, SelectableValue } from '@grafana/data';
 import { MultiSelect } from '@grafana/ui';
 
-interface State<T> {
-  isLoading: boolean;
-  options: Array<SelectableValue<T>>;
-}
-
 type Props<T> = FieldConfigEditorProps<T[], SelectFieldConfigSettings<T>>;
+type OptionsState<T> = Array<SelectableValue<T>>;
 
-// todo can this be a function component?
-export class MultiSelectValueEditor<T> extends React.PureComponent<Props<T>, State<T>> {
-  state: State<T> = {
-    isLoading: true,
-    options: [],
-  };
+export const MultiSelectValueEditor = <T extends object>(props: Props<T>) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [options, setOptions] = useState<OptionsState<T>>([]);
 
-  componentDidMount() {
-    this.updateOptions();
-  }
+  useEffect(() => {
+    updateOptions();
+  }, [props.item?.settings, props.context?.data]);
 
-  componentDidUpdate(oldProps: Props<T>) {
-    const old = oldProps.item?.settings;
-    const now = this.props.item?.settings;
-    if (old !== now) {
-      this.updateOptions();
-    } else if (now?.getOptions) {
-      const old = oldProps.context?.data;
-      const now = this.props.context?.data;
-      if (old !== now) {
-        this.updateOptions();
-      }
-    }
-  }
-
-  updateOptions = async () => {
-    const { item } = this.props;
+  const updateOptions = async () => {
+    const { item } = props;
     const { settings } = item;
-    let options: Array<SelectableValue<T>> = item.settings?.options || [];
+    let _options: Array<SelectableValue<T>> = item.settings?.options || [];
     if (settings?.getOptions) {
-      options = await settings.getOptions(this.props.context);
+      _options = await settings.getOptions(props.context);
     }
-    if (this.state.options !== options) {
-      this.setState({
-        isLoading: false,
-        options,
-      });
+    if (options !== _options) {
+      setOptions(_options);
+      setIsLoading(false);
     }
   };
 
-  render() {
-    const { options, isLoading } = this.state;
-    const { value, onChange, item } = this.props;
-
-    const { settings } = item;
-    return (
-      <MultiSelect<T>
-        isLoading={isLoading}
-        value={value}
-        defaultValue={value}
-        allowCustomValue={settings?.allowCustomValue}
-        onChange={e => onChange(e.map(v => v.value as T))}
-        options={options}
-      />
-    );
-  }
-}
+  const { value, onChange, item } = props;
+  const { settings } = item;
+  return (
+    <MultiSelect<T>
+      isLoading={isLoading}
+      value={value}
+      defaultValue={value}
+      allowCustomValue={settings?.allowCustomValue}
+      onChange={e => onChange(e.map(v => v.value as T))}
+      options={options}
+    />
+  );
+};
