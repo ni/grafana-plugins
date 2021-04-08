9ea26f75c2920389356f05f61db68990993765eb
mure <carsonatxmoore@gmail.com>
2021-04-08 13:23:09 -0500

    Grafana provisioning

--- Setup-Grafana.ps1
+++ Setup-Grafana.ps1
@@ -1,6 +1,7 @@
-$FilesRoot = "C:/Program Files/National Instruments/Shared"
+$FilesRoot = 'C:/Program Files/National Instruments/Shared'
+$ConfigRoot = 'C:/ProgramData/National Instruments/Skyline'
 $PluginRoot = "$FilesRoot/Web Server/htdocs/plugins"
-$GrafanaRoot = "C:/Program Files/GrafanaLabs/grafana"
+$GrafanaRoot = 'C:/Program Files/GrafanaLabs/grafana'
 
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
 {
@@ -20,11 +21,13 @@ if (-Not $IsInstalled) {
 }
 
 Write-Output 'Writing SystemLink configuration'
-Copy-Item -Path './ni-grafana' -Destination $PluginRoot -Recurse -Force
-Copy-Item -Path './custom.ini' -Destination "$GrafanaRoot/conf/" -Force
-Copy-Item -Path './75_grafana.conf' -Destination "$FilesRoot/Web Server/conf/conf.d"
+Copy-Item -Path './config/ni-grafana' -Destination $PluginRoot -Recurse -Force
+Copy-Item -Path './config/custom.ini' -Destination "$GrafanaRoot/conf" -Force
+Copy-Item -Path './config/75_grafana.conf' -Destination "$FilesRoot/Web Server/conf/conf.d" -Force
 
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
 $PluginsUrl = 'https://api.github.com/repos/ni-kismet/grafana-plugins/releases/latest'
+
 # If this breaks, contact Carson Moore
 $Headers = @{ Authorization = 'Basic bXVyZTpnaHBfc3JsT1dNaDFIcTUyT2Uxank0TE5hR2V2SzV1c2hVM3czeVNF' }
 Write-Output 'Downloading SystemLink Grafana plugins'
@@ -37,6 +40,12 @@ Invoke-RestMethod `
 Expand-Archive 'grafana-plugins.zip' -Force
 Write-Output 'Copying over plugins to Grafana'
 Copy-Item -Path './grafana-plugins/*' -Destination "$GrafanaRoot/data/plugins" -Recurse -Force
+
+Write-Output 'Provisioning datasources'
+Copy-Item -Path './examples/*.ipynb' -Destination "$ConfigRoot/JupyterHub/notebooks/_shared/reports" -Force
+Copy-Item -Path './config/datasources.yml' -Destination "$GrafanaRoot/conf/provisioning/datasources" -Force
+Copy-Item -Path './examples/Default Test Module.json' -Destination "$ConfigRoot/Grafana/dashboards" -Force
+Copy-Item -Path './config/dashboards.yml' -Destination "$GrafanaRoot/conf/provisioning/dashboards" -Force
 
 Write-Output 'Restarting services...'
 Restart-Service 'Grafana'
