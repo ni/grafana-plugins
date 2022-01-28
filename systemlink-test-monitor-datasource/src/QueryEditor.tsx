f00471aa89be600a777ae8d8d328d4413672933f
Carson Moore <carson.moore@ni.com>
2022-01-28 17:08:37 -0600

    Linting

--- systemlink-test-monitor-datasource/src/QueryEditor.tsx
+++ systemlink-test-monitor-datasource/src/QueryEditor.tsx
@@ -13,17 +13,19 @@ export class QueryEditor extends PureComponent<Props> {
     const query = defaults(this.props.query, defaultQuery);
     console.log(query);
     return (
-      <Card
-        heading='Alert'
-        description='This datasource is only intended to be used for variable queries.'>
-          <Card.Figure>
-            <Icon name='exclamation-triangle' size='xl'/>
-          </Card.Figure>
-          <Card.Actions>
-            <a target='_blank' href='https://grafana.com/docs/grafana/latest/variables/variable-types/add-query-variable/'>
-              <Button>More info</Button>
-            </a>
-          </Card.Actions>
+      <Card heading="Alert" description="This datasource is only intended to be used for variable queries.">
+        <Card.Figure>
+          <Icon name="exclamation-triangle" size="xl" />
+        </Card.Figure>
+        <Card.Actions>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://grafana.com/docs/grafana/latest/variables/variable-types/add-query-variable/"
+          >
+            <Button>More info</Button>
+          </a>
+        </Card.Actions>
       </Card>
     );
   }
