3d2793c0a72e8c95c67ed4cca76b56ed72c1baf7
Christopher Stryker <chris.stryker@ni.com>
2024-01-11 00:03:27 -0600

    update build script to pull in code from submodule

--- build.sh
+++ build.sh
@@ -1,17 +1,39 @@
 #!/usr/bin/env bash
 set -e
 
+external_datasources=("workspace" "system" "tag")
+external_panels=("plotly")
+local_plugins=("systemlink-test-monitor-datasource" "systemlink-notebook-datasource")
+
 rm -rf build
 mkdir -p build/{plugins,config,examples}
+mkdir -p systemlink-grafana-plugins/dist
 
-for d in ./*/; do
-    cd $d
-    if test -f package.json; then
-        echo $(pwd)
-        npm ci
-        npm run build
-        cp -r dist ../build/plugins/${d}
-    fi
+cd systemlink-grafana-plugins
+
+npm ci
+npm run build
+
+for n in ${external_datasources[@]}; 
+do
+    cp -r dist/datasources/${n} ../build/plugins/${n}
+done
+
+for p in ${external_panels[@]};
+do
+    cp -r dist/panels/${p} ../build/plugins/${p}
+done
+
+cd ..
+
+for d in ${local_plugins[@]};
+do
+    cd ${d}
+	echo $(pwd)
+	npm ci
+	npm run build
+	cp -r dist ../build/plugins/${d}
+    
     cd ..
 done
 
